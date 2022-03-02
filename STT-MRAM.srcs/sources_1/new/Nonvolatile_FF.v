33c2706567aa033685507a2e311f4b8afcdd2aab
feedingmasterHE <uqdxz.zhuofan@gmail.com>
2022-03-02 15:30:02 +0100

    123

---
+++ STT-MRAM.srcs/sources_1/new/Nonvolatile_FF.v
@@ -0,0 +1,60 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2022/02/21 15:32:26
+// Design Name: 
+// Module Name: Nonvolatile_FF
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module nonvolatile_ff(
+    input clk,
+    input rst,
+    input d,
+    input read_mtj,
+    input write_mtj,
+    output reg q
+    );
+    
+    reg store_MTJ;
+    
+    //write process in the MTJ corresponding to the signal write_mtj
+    always@(posedge clk,negedge rst) begin
+        if(!rst)
+            store_MTJ <= 1'b0;
+        else begin
+            if(write_mtj) 
+                store_MTJ <= d;
+            else 
+                store_MTJ <= store_MTJ;
+        end
+           
+    end
+    //read process
+    always@(posedge clk,negedge rst) begin
+        if(!rst) 
+            q <= 1'b0;
+        else begin
+            if(read_mtj == 1)
+                q <= store_MTJ;
+            else
+                q <= d;
+        end
+    end
+   
+    
+    
+endmodule
